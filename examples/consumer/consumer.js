0536b25b4285cbec1f945868e913fe535dd6ce4b
Injo Luuvish Hwang <luuvish@gmail.com>
2011-12-23 00:31:46 +0900

    oauthware router function refactored

---
+++ examples/consumer/consumer.js
@@ -0,0 +1,82 @@
+
+/*!
+ * ThingsUp
+ * Copyright(c) 2011 CodeHolics Inc.
+ * Copyright(c) 2011 luuvish
+ * CodeHolics Licensed
+ */
+
+/**
+ * Module dependencies.
+ */
+
+var oauthware = require('../../index'),
+    connect   = require('connect'),
+    parse     = require('url').parse,
+    fs        = require('fs');
+
+/**
+ * Web client server parameters
+ *
+ * hostname: host server domain name
+ * port:     http port number
+ * service:  subdomain name
+ *
+ * server listens to http://hostname:port/service/..
+ */
+
+var serverUrl = 'http://127.0.0.1:8020/',
+    url       = parse(serverUrl),
+    protocol  = url.protocol || 'http:',
+    hostname  = url.hostname || '127.0.0.1',
+    port      = url.port     || (protocol === 'https:' ? '443' : '80'),
+    pathname  = url.pathname || '/',
+    server;
+
+// catch Exception
+
+process.on('uncaughtException', function (err) {
+  console.log('Caught exception: ' + err.stack);
+});
+
+/**
+ * router hostname:port/domain/.. to /public/.. directory
+ */
+
+if (protocol === 'https:') {
+  server = connect.createServer({
+    key  : fs.readFileSync(__dirname + '/server.key'),
+    cert : fs.readFileSync(__dirname + '/server.crt')
+  });
+} else {
+  server = connect.createServer();
+}
+
+//server.use(pathname, connect.logger());
+
+server.use(pathname, connect.favicon())
+      .use(pathname, connect['static'](__dirname + '/public'))
+      .use(pathname, connect.cookieParser())
+      .use(pathname, connect.session({
+        secret : 'FlurbleGurgleBurgle', 
+        store  : new connect.session.MemoryStore({ reapInterval: -1 })
+      }))
+      .use(pathname, oauthware.router({
+        'Twitter' : {
+          mount          : '/auth/twitter',
+          consumerKey    : 'uRnESc07dpIGdBDVc1V7A',
+          consumerSecret : 'vB3t0xlZgyQdenJGh59rvlagd7rTfsdX5ddeCuAIwTo'      
+        },
+        'Facebook' : {
+          mount          : '/auth/facebook',
+          appId          : '214990631897215',
+          appSecret      : '236d093791188ff7bed07a817a63e53a',
+          scope          : 'email'
+        }
+      }));
+
+// listen to http://hostname:port
+
+server.listen(port, hostname);
+
+console.log('listen to ' + protocol + '//' + hostname + ':' + port + pathname);
